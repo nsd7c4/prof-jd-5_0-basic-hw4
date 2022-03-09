943357007ef2a70ecef7e1935c34c8a3f0b170bb
rashidgafurov <nsd7c4@gmail.com>
2022-03-09 14:37:17 -0500

    Homework 4 assignment 1

--- src/com/RashidGafurov_INC_LOL/Main.java
+++ src/com/RashidGafurov_INC_LOL/Main.java
@@ -3,6 +3,17 @@ package com.RashidGafurov_INC_LOL;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //Assignment 1
+        int i = 10;
+        while (i>0) {
+            System.out.print(i + " ");
+            i -= 1;
+        }
+
+        System.out.println("");
+
+        for (i = 1; i <= 10; i++){
+            System.out.print(i + " ");
+        };
     }
 }
