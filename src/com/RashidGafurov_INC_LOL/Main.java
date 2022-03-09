9ef2e7cbbefcbd41a9a54785a6e01b912fcefb38
rashidgafurov <nsd7c4@gmail.com>
2022-03-09 15:23:49 -0500

    Homework 4 assignment 3

--- src/com/RashidGafurov_INC_LOL/Main.java
+++ src/com/RashidGafurov_INC_LOL/Main.java
@@ -5,14 +5,14 @@ public class Main {
     public static void main(String[] args) {
         //Assignment 1
         int i = 10;
-        while (i>0) {
+        while (i > 0) {
             System.out.print(i + " ");
             i -= 1;
         }
 
         System.out.println("");
 
-        for (i = 1; i <= 10; i++){
+        for (i = 1; i <= 10; i++) {
             System.out.print(i + " ");
         }
         System.out.println("");
@@ -24,5 +24,15 @@ public class Main {
             System.out.println("Today is Friday the " + firstFriday + "th. Prepare the report!");
             firstFriday += 7;
         } while (firstFriday <= 31);
+
+        //Assignment 3
+        int firstYear = 2022 - 200;
+        int lastYear = 2022 + 100;
+        for (i=firstYear; i < lastYear; i++) {
+            while (i % 79 == 0 && i < lastYear) {
+                System.out.println(i);
+                i += 79;
+            }
+        }
     }
 }
