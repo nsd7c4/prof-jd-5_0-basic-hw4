5bc8929016dc4a17f1212c25bcb6a9196dfb9d0a
rashidgafurov <nsd7c4@gmail.com>
2022-03-11 09:35:22 -0500

    Homework 4 assignment 4 and 5

--- src/com/RashidGafurov_INC_LOL/Main.java
+++ src/com/RashidGafurov_INC_LOL/Main.java
@@ -9,7 +9,6 @@ public class Main {
             System.out.print(i + " ");
             i -= 1;
         }
-
         System.out.println("");
 
         for (i = 1; i <= 10; i++) {
@@ -28,8 +27,33 @@ public class Main {
         //Assignment 3
         int firstYear = 2022 - 200;
         int lastYear = 2022 + 100;
-        for (i = (firstYear/79+1)*79; i < lastYear; i+=79) {
-                System.out.println(i);
+        for (i = (firstYear / 79 + 1) * 79; i < lastYear; i += 79) {
+            System.out.println(i);
+        }
+
+        //Assignment 4
+        for (i = 1; i <= 30; i++) {
+            System.out.print(i + ":");
+            if (i % 3 == 0) {
+                System.out.print(" ping");
+            }
+            if (i % 5 == 0) {
+                System.out.print(" pong");
+            }
+            System.out.println("");
+        }
+
+        //Assignment 5
+        int firstNumber = 0;
+        int secondNumber = 1;
+        int tempV = 0;
+        System.out.print(firstNumber + " ");
+        System.out.print(secondNumber + " ");
+        for (i = 1; i <= 8; i++) {
+            tempV = secondNumber;
+            secondNumber += firstNumber;
+            firstNumber = tempV;
+            System.out.print(secondNumber + " ");
         }
     }
 }
